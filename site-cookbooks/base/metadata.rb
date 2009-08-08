58d87eb2fda2c4c0ad71e97f2e5b6f3c99ef948c
Geoff Longman <glongman@overlay.tv>
2009-08-08 12:07:37 -0400

    flrbbtt

--- site-cookbooks/base/metadata.rb
+++ site-cookbooks/base/metadata.rb
@@ -5,4 +5,9 @@ description      "Installs/Configures base"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
 
+depends "build-essential"
+depends "subversion"
+depends "git-core"
+depends "checkinstall"
+
 recipe "base", "base configuration"
