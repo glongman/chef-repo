e7fd370f98495319e6d6c8a10dc5e9016a201127
Geoff Longman <glongman@overlay.tv>
2009-08-08 13:24:48 -0400

    install ffmpeg

--- site-cookbooks/ffmpeg/recipes/default.rb
+++ site-cookbooks/ffmpeg/recipes/default.rb
@@ -6,3 +6,40 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe "ffmpeg:dependencies"
+
+bash "install ffmpeg" do
+  cwd '/tmp'
+  code <<-EOH
+    svn checkout svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg 
+    cd ffmpeg
+    ./configure --enable-gpl \
+        --enable-nonfree \
+        --enable-pthreads \
+        --enable-static \
+        --enable-shared \
+        --enable-libgsm \
+        --enable-libfaac \
+        --enable-libfaad \
+        --enable-libfaadbin\
+        --enable-libmp3lame \
+        --enable-libtheora \
+        --enable-libvorbis \
+        --enable-postproc \
+        --enable-libx264 \
+        --enable-libxvid \
+        --enable-x11grab \
+        --enable-libopencore-amrnb \
+        --enable-version3\
+        --disable-devices\
+        --disable-ffserver\
+        --disable-ffplay\
+        --disable-debug\
+        --disable-network
+    make
+    checkinstall --fstrans=no --install=yes --pkgname=ffmpeg --pkgversion '3:0.svn`date +%Y%m%d`-12ubuntu3' --default
+    ldconfig
+    EOH
+  action :run
+end
